b7de77ae53082b5414b5ea36ca671323e3dae690
mohamed tergui <simotergui4@gmail.com>
2024-04-06 04:31:34 +0200

    add grade controller

---
+++ app/Http/Controllers/Api/GradeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Grade;
+use Illuminate\Http\Request;
+use App\Repositories\GradeRepositoryInterface;
+
+class GradeController extends Controller
+{
+    private $repository;
+    function __construct(GradeRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+    function index()
+    {
+        return $this->success($this->repository->paginate(10));
+    }
+
+    function store(Request $request)
+    {
+        return $this->success($this->repository->create($request->all()), "Grade added with success");
+    }
+
+
+    function update(Request $request, Grade $Grade)
+    {
+        return $this->success($this->repository->update($Grade, $request->all()), "Grade updated with success");
+    }
+
+
+    function destroy(Grade $Grade)
+    {
+        $this->repository->delete($Grade);
+        return $this->success([], "Grade deleted with success");
+    }
+
+}
