35f6825dde8aeb7ee90655ea8abb535e829aa91b
mohamed tergui <simotergui4@gmail.com>
2024-04-26 12:36:06 +0200

    add function for update  profile

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -19,7 +19,47 @@ class ProfileController extends ExamController
         $id = Auth::id();
         return $this->success($this->repository->update($this->repository->getById($id),$request->all()),"profile updated with success"); 
     } 
-    function changePassword(Request $request){
+    function updateProfileImage(Request $request){
+        $id = Auth::id();
+        $user = $this->repository->getById($id);
+        if ($request->hasFile("image")) {
 
+            $file = $request->file('image');
+            $extension = $file->getClientOriginalExtension();
+            $fileName = time() . '.' . $extension;
+            $path = 'uploads/students/';
+
+
+            $oldImage = $user->image ?? null;
+
+            $file->move($path, $fileName);
+
+
+            $user = $this->repository->update($user, ["image" => $fileName]);
+
+
+            if ($oldImage) {
+                $oldImagePath = $path . $oldImage;
+                if (file_exists($oldImagePath)) {
+                    unlink($oldImagePath);
+                }
+            }
+
+            return $this->success($user, "Image updated successfully");
+        } else if ($request->image == "") {
+             
+            $oldImage = $user->image ?? null;
+            $path = 'uploads/students/';
+            $user = $this->repository->update($user, ["image" => null]);
+            if ($oldImage) {
+                $oldImagePath = $path . $oldImage;
+                if (file_exists($oldImagePath)) {
+                    unlink($oldImagePath);
+                }
+            }
+            return $this->success($user, "Image deleted successfully");
+
+        }
+        else return   $this->success($user);
     }
 }
